d22e8adddff638c6d5b8a709e338411fc8232b21
Dilawar Khan <dilawar2030@gmail.com>
2024-06-02 02:55:39 +0500

    Add game logic to decide who won and increment score

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,7 +5,8 @@ function getComputerChoice() {
     return Math.floor((Math.random() * 3) + 1)
 }
 
-console.log(getComputerChoice());
+// getComputerChoice() tester
+// console.log(getComputerChoice());
 
 function getHumanChoice() {
 
@@ -44,4 +45,44 @@ function getHumanChoice() {
     return humanChoice
 }
 
-console.log(getHumanChoice());
+// getHumanChoice() tester
+// console.log(getHumanChoice());
+
+function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
+
+    console.log("play");
+    if(humanChoice === computerChoice) {
+        console.log("You drew!");
+        humanScore++;
+        computerScore++;
+    }
+    else if (humanChoice === 1 && computerChoice === 3) {
+        console.log("You won! Rock beats Scissors");
+        humanScore++;
+    }
+    else if (humanChoice === 2 && computerChoice === 1) {
+        console.log("You won! Paper beats Rock");
+        humanScore++;
+    }
+    else if (humanChoice === 3 && computerChoice === 2) {
+        console.log("You won! Scissors beat Paper");
+        humanScore++;
+    }
+    else if (computerChoice === 1 && humanChoice === 3) {
+        console.log("You Lost! Rock beats Scissors");
+        computerScore++;
+    }
+    else if (computerChoice === 2 && humanChoice === 1) {
+        console.log("You Lost! Paper beats Rock");
+        computerScore++;
+    }
+    else if (computerChoice === 3 && humanChoice === 2) {
+        console.log("You Lost! Scissors beat Paper");
+        computerScore++;
+    }
+
+    console.log(`Computer Score: ${computerScore}\nHuman Score: ${humanScore}`);
+}
+
+// playRound() tester
+// playRound();
